309a722fa06566712cb8afc53b432caea92f300a
Diego Doná <diedona@gmail.com>
2019-11-13 19:05:50 -0300

    factory for decisions

---
+++ DDona.DesignPatterns.Factory.Domain/Enums/ESituationType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDona.DesignPatterns.Factory.Domain.Enums
+{
+    public enum ESituationType
+    {
+        Strength = 0,
+        Stealth = 1,
+        Intelligence = 2
+    }
+}
---
+++ DDona.DesignPatterns.Factory.Domain/Factories/HeroFactory.cs
@@ -0,0 +1,24 @@
+﻿using DDona.DesignPatterns.Factory.Domain.Enums;
+using DDona.DesignPatterns.Factory.Domain.Factories.Base;
+using DDona.DesignPatterns.Factory.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDona.DesignPatterns.Factory.Domain.Factories
+{
+    public class HeroFactory
+    {
+        private readonly Dictionary<ESituationType, HeroFactoryBase> _factories;
+
+        public HeroFactory()
+        {
+            _factories = new Dictionary<ESituationType, HeroFactoryBase>()
+            {
+                { ESituationType.Intelligence, new WonderWomanFactory() },
+                { ESituationType.Stealth, new BatmanFactory() },
+                { ESituationType.Strength, new SupermanFactory() }
+            };
+        }
+    }
+}
